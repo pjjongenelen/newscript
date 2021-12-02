d824cae3c1cec1a5fdea7a5ddf73dfb527801937
Tim Jongenelen <timjongenelen@hotmail.com>
2021-12-02 18:19:51 +0100

    Updated version of preprocessed data, columns have been split out, and text is converted to truecase

--- src/preprocessing/muc_pp.py
+++ src/preprocessing/muc_pp.py
@@ -4,6 +4,7 @@ Preprocessing steps taken on the MUC data file as received from Nguyen
 
 import os
 import pandas as pd
+from truecase import get_true_case
 
 INPUT_PATH = os.getcwd() + "\data\muc-dev-tst1-4"
 OUTPUT_PATH = os.getcwd() + "\processed_data\muc.csv"
@@ -15,8 +16,11 @@ def load_data(path: str):
     return pd.read_csv(path, sep = '\t', names = ['muc_id', 'location'])  
 
 def fix_seperators(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix mmiss a seperator between the date and article text"""
+
     # find the rows without seperator hyphens
     seperator_errors = [index for index, row in df.iterrows() if " -- " not in row['location']]
+    
     # add the hyphens
     for x in seperator_errors:
         df.loc[x]['location'] = df.loc[x]['location'][:17] + " --" + df.loc[x]['location'][17:]
@@ -25,17 +29,20 @@ def fix_seperators(df: pd.DataFrame) -> pd.DataFrame:
     
     
 def split_column(df, col1, col2, sep):
+    "Splits textual content of a df column based on a separator"
+
     df[col1], df[col2] = zip(*[(d[0], d[1]) for d in [c.split(sep, 1) for c in df[col1]]])
     return df
 
-
-
 def main():
+    """Preprocess the MUC data that we got from Nguyen"""
+
     path = INPUT_PATH
     muc_data = load_data(path)
     muc_data = fix_seperators(muc_data)
     muc_data = split_column(df = muc_data, col1='location', col2='date', sep=', ')
     muc_data = split_column(df = muc_data, col1='date', col2='text', sep = ' -- ')
+    muc_data['text'] = [get_true_case(text) for text in muc_data['text']]
 
     muc_data.to_csv(OUTPUT_PATH)
 
